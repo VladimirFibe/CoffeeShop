0ea568aa153a15f617331e0a53a3b363996073c9
Vladimir <motiw@icloud.com>
2023-05-07 22:25:58 +0600

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Vladimir on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
